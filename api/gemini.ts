1021564656fd78a0a81054dd79e57faba71dea85
viqelt <oraed205@gmail.com>
2026-03-21 01:08:28 +0100

    Update gemini.ts

--- api/gemini.ts
+++ api/gemini.ts
@@ -1,4 +1,4 @@
-export const config = { runtime: "edge" };
+export const config = { runtime: "nodejs" };
 
 const GEMINI_KEY = "AIzaSyAOYeIaC9pN4y-VMgx83uVqP-qqFwqyFHc";
 const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_KEY}`;
